c0b4551fa6a594956ceb8527ada38b68884fa548
faymanns <florian.aymanns@epfl.ch>
2020-02-13 17:32:00 +0100

    added ffmpeg interface

--- plotting_utils/main.py
+++ plotting_utils/main.py
@@ -1,4 +1,5 @@
 import itertools
+import subprocess
 
 import cv2
 from tqdm import tqdm
@@ -7,7 +8,7 @@ from .utils import *
 
 
 def make_video(video_path, frame_generator, fps, output_shape=(-1, 2880)):
-    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     first_frame = next(frame_generator)
     frame_generator = itertools.chain([first_frame], frame_generator)
     output_shape = resize_shape(output_shape, first_frame.shape[:2])
@@ -19,3 +20,23 @@ def make_video(video_path, frame_generator, fps, output_shape=(-1, 2880)):
         video_writer.write(rgb)
 
     video_writer.release()
+
+
+def ffmpeg(command, pixel_format="yuv420p"):
+    """
+    Interface to run ffmpeg from python.
+
+    Parameters
+    ----------
+    command : string
+        command passed to ffmpeg.
+    pixel_format : string
+        Specifies the pixel format of the output video.
+        If `command` includes '-pix_fmt', `pixel_format
+        is ignored. Default is 'yuv420p' which ensures
+        videos can be included in presentation.
+    """
+    command_list = command.split()
+    if not "-pix_fmt" in command_list:
+        command_list = command_list[:-1] + ["-pix_fmt", pixel_format] + [command_list[-1],]
+    subprocess.Popen(["ffmpeg",] + command_list)

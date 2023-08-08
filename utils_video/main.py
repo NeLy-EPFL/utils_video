9d0553467b9bfde5c305039892c372c881acfd5b
Gizem Ozdil <pembe.ozdil@epfl.ch>
2023-08-08 18:08:23 +0200

    minor updates

--- utils_video/main.py
+++ utils_video/main.py
@@ -42,6 +42,36 @@ def make_video(video_path, frame_generator, fps, output_shape=(-1, 2880), n_fram
 
     video_writer.release()
 
+def process_frame(frame_img, output_shape=(-1, 1000)):
+    frame, img = frame_img
+    resized = cv2.resize(img, output_shape[::-1])
+    rgb = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
+    return (frame, rgb)
+
+def make_video_parallel(video_path, frame_generator, fps, output_shape=(-1, 1000), n_frames=-1):
+    """ Runs make_video in parallel."""
+    from multiprocessing import Pool
+    from functools import partial
+
+    if float(fps).is_integer() and int(fps) != 1 and (int(fps) & (int(fps) - 1)) == 0:
+        import warnings
+
+        warnings.warn(
+            f"Frame rate {fps} is a power of 2. This can result in faulty video files."
+        )
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    first_frame = next(frame_generator)
+    frame_generator = itertools.chain([first_frame], frame_generator)
+    output_shape = resize_shape(output_shape, first_frame.shape[:2])
+    video_writer = cv2.VideoWriter(video_path, fourcc, fps, output_shape[::-1])
+
+    with Pool() as pool:
+        for _, rgb in pool.map(partial(process_frame, output_shape=output_shape), frame_generator, chunksize=1):
+            video_writer.write(rgb)
+
+    video_writer.release()
+
+
 
 def ffmpeg(command, pixel_format="yuv420p"):
     """
